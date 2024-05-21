2100236fa5a162c9a7e3ea4af64778b0e4e4d792
Jaro <pompabicki69@gmail.com>
2024-05-21 10:46:38 +0200

    task 2

--- git_lab789.py
+++ git_lab789.py
@@ -14,3 +14,14 @@ def read_file(file_path):
             return yaml.safe_load(file)
         else:
             raise ValueError(f"Unsupported file format: {file_path}")
+def write_file(data, file_path):
+    with open(file_path, 'w') as file:
+        if file_path.endswith('.json'):
+            json.dump(data, file, indent=4)
+        elif file_path.endswith('.xml'):
+            xml_string = xmltodict.unparse(data, pretty=True)
+            file.write(xml_string)
+        elif file_path.endswith('.yaml') or file_path.endswith('.yml'):
+            yaml.dump(data, file, default_flow_style=False)
+        else:
+            raise ValueError(f"Unsupported file format: {file_path}")
