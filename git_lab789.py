3dd2a8f481746513562628f7571c5be37336cad9
Jaro <pompabicki69@gmail.com>
2024-05-21 10:45:46 +0200

    task 2

--- git_lab789.py
+++ git_lab789.py
@@ -4,3 +4,13 @@ import sys
 import json
 import yaml
 import xmltodict
+def read_file(file_path):
+    with open(file_path, 'r') as file:
+        if file_path.endswith('.json'):
+            return json.load(file)
+        elif file_path.endswith('.xml'):
+            return xmltodict.parse(file.read())
+        elif file_path.endswith('.yaml') or file_path.endswith('.yml'):
+            return yaml.safe_load(file)
+        else:
+            raise ValueError(f"Unsupported file format: {file_path}")
